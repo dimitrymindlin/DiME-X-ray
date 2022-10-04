9c346ccd8e2e6085c875c038a076e7e8e5078faa
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-04 17:10:33 +0200

    Relocated paths and files.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond False --diffusion_steps 500 --learn_sigma True --noise_schedule linear --num_channels 128 --num_heads 4 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
 SAMPLE_FLAGS="--batch_size 50 --timestep_respacing 200"
-DATAPATH=/home/personnels/jeanner211/DATASETS/celeba
+DATAPATH=/Users/dimitrymindlin/UniProjects/DiME-X-ray/img_align_celeba
 OUTPUT_PATH=results
 EXPNAME=test
 
@@ -18,7 +18,7 @@ QUERYLABEL=31
 TARGETLABEL=-1
 IMAGESIZE=128  # dataset shape
 
-python -W ignore ../main.py $MODEL_FLAGS $SAMPLE_FLAGS \
+python -W ignore main.py $MODEL_FLAGS $SAMPLE_FLAGS \
   --query_label $QUERYLABEL --target_label $TARGETLABEL \
   --output_path $OUTPUT_PATH \
   --start_step $S --dataset 'CelebA' \
